f40fcd0be91ac370fca82ec8c38b18694119b61c
Manuel <mygamezapp@gmail.com>
2025-12-06 21:28:04 +0100

    initial commit

---
+++ src/main/java/de/manuel_source/godocards/GodocardsApplication.java
@@ -0,0 +1,13 @@
+package de.manuel_source.godocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodocardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodocardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/manuel_source/godocards/GodocardsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.manuel_source.godocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodocardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
