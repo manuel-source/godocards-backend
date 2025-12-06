03b89a1cf1349a179372b3e513a022572a6f52be
Manuel <mygamezapp@gmail.com>
2025-12-06 21:52:17 +0100

    integrates pokeapi v2

--- src/main/java/de/manuel_source/godocards/GodocardsApplication.java
+++ src/main/java/de/manuel_source/godocards/GodocardsApplication.java
@@ -2,8 +2,10 @@ package de.manuel_source.godocards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GodocardsApplication {
 
 	public static void main(String[] args) {
