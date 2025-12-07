81165436c4a17a127dfb25c69f64bae061e92902
Manuel <mygamezapp@gmail.com>
2025-12-07 21:05:45 +0100

    adds github-action build-and-test.yml

--- src/main/java/de/manuel_source/godocards/entity/Product.java
+++ src/main/java/de/manuel_source/godocards/entity/Product.java
@@ -1,11 +1,9 @@
 package de.manuel_source.godocards.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.math.BigDecimal;
